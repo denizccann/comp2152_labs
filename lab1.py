1c87d9f1b675df8c440e3ccd09fca02cde56e8e4
Deniz Can <denizccann@gmail.com>
2026-01-18 16:23:20 -0500

    Final version with saved code

---
+++ lab1.py
@@ -0,0 +1,18 @@
+# Sample Coding Questions 01 Week 01
+# Name: Deniz Can
+
+# 1. Defining Variables
+numbers = [1, 4, 7, 9]
+
+# 2. Order of Operations
+a, b, c, d = 1, 2, 3, 4
+e = (a - ((b ** c) // d)) + (a % c)
+
+# 3. Formatting
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# 4. Common Functions
+user_input = input("Please enter your age: ")
+user_age = int(user_input) + 22
+print("Now showing the shop items filtered by age:", user_age)
